1bc5b5494bdd9a62f3d2980bb0cfa3a402480fa3
Jhon Cardenas <alejandrotareas029@gmail.com>
2025-11-27 19:39:03 -0500

    v0.1_CreacionProyectoFintrix_paquetesJava_JhonCardenas_27112025

---
+++ fintrix/src/test/java/com/fintrix/fintrix/FintrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintrix.fintrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/fintrix/fintrix/FintrixApplication.java
@@ -0,0 +1,13 @@
+package com.fintrix.fintrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintrixApplication.class, args);
+	}
+
+}
